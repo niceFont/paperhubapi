83bb22e9080de318b6b4da9d84674ef81ee386fc
nicefont <peterwilms@hotmail.de>
2020-05-09 18:38:52 +0200

    more and more deploy fixes

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-mix deps.get 
+
+ELIXIR_DIR="/home/paperhubapi"
+
+cd $ELIXIR_DIR
+
+mix local.hex --force
+mix deps.get
 mix ecto.setup
+mix ecto.migrate
+
+exit $?
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-mix phx.server & echo $! > ./serverpid
+ELIXIR_DIR="/home/paperhubapi"
+
+cd $ELIXIR_DIR
+
+killall -2 beam.smp
+sleep 1
+
+. ./.env
+elixir --detached -S mix phx.server
+
+exit $?
