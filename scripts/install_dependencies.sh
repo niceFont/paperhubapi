762c9a04c2100c74e4aaa7963c90a2a4d431a0e6
nicefont <peterwilms@hotmail.de>
2020-05-09 18:51:57 +0200

    fix

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-ELIXIR_DIR="/paperhubapi"
-
-cd $ELIXIR_DIR
 
 mix local.hex --force
 mix deps.get
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-ELIXIR_DIR="/paperhubapi"
-
-cd $ELIXIR_DIR
-
 killall -2 beam.smp
 sleep 1
 
