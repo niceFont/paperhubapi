2268b91b18323c9e52f4626255ad06d530d22744
niceFont <peterwilms@hotmail.de>
2020-05-16 00:32:08 +0200

    deploy test

---
+++ scripts/before_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo killall -9 beam.smp
+sudo rm -rf /paperhubapi
+
+exit $?
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -6,6 +6,6 @@ killall -2 beam.smp
 sleep 1
 
 . ./.env
-elixir --detached -S mix phx.server
+mix phx.server &
 
 exit $?
